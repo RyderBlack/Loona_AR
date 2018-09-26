b56d701596e3897ce03f8325c5bfdb676cb295eb
RyderBlack <ryhad.boughanmi@gmail.com>
2018-09-26 15:13:47 -0400

    Initial Commit

---
+++ Loona_cloud_ARTests/Loona_cloud_ARTests.swift
@@ -0,0 +1,34 @@
+//
+//  Loona_cloud_ARTests.swift
+//  Loona_cloud_ARTests
+//
+//  Created by Royalty on 26/09/2018.
+//  Copyright © 2018 Royalty Inc. All rights reserved.
+//
+
+import XCTest
+@testable import Loona_cloud_AR
+
+class Loona_cloud_ARTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
